c6f48621e40ac1374f264a9efd42b4fd0ab7e0f6
nidahansari <149885925+nidahansari@users.noreply.github.com>
2025-09-29 14:16:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,22 @@ for message in st.sessions_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+prompt = st.chat_input("Hello! How may I help you today?")
+if prompt:
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+st.session_state.messages.append({"role": "user", "content": prompt})
+
+with st.chat_message("assistant"):
+    message_placeholder = st.empty()
+    full_response = ""
+    for response in openai.ChatCompletion.create(
+        model=st.session_state["openai_model"],
+        messages=[
+            {"role": ,["role"], "content": ,["content"]}
+            for m in st.session_state.messages
+    ],
+    stream=True,
+):
+    full_response += response.choices[0]
