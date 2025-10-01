ce9146db3afb07ca135ce2a06f862a76aac03cdd
nidahansari <149885925+nidahansari@users.noreply.github.com>
2025-09-30 20:46:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 from openai import OpenAI
 import streamlit as st
 
-client = OpenAI(api_key="sk-...tQsA")
+# Read key from Streamlit secrets
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
 
 st.title("TigerChat 🐯")
 
