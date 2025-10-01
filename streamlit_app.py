a281e2b5b13f69dda1af5658b0fe280248f6aaf7
nidahansari <149885925+nidahansari@users.noreply.github.com>
2025-09-30 20:38:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 import streamlit as st
 
 # ⚠️ Replace with your actual key
-client = OpenAI(api_key="sk-xxxxxxx")
+client = OpenAI(api_key="sk-...tQsA")
 
 st.title("TigerChat 🐯")
 
